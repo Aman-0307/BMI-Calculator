7fe87fccfd55bae4df301dbc5c8942ee7bddb4bc
Aman Yadav <56191587+Aman32403@users.noreply.github.com>
2021-02-18 16:58:53 +0530

    BMI Cal and D to F

---
+++ BMI/bmical.js
@@ -0,0 +1,16 @@
+//BMI calculator
+
+const bmical = (w,h)=>{
+    let bmi = 703*w/(h*h);
+    console.log(bmi);
+}
+bmical(68,70);
+
+
+//degree to fahrenheit conversion
+
+const DtoF = (celsius)=>{
+    let DtoF = celsius*9/5+32;
+    console.log(DtoF);
+}
+DtoF(60);
